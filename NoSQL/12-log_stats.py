b1847a3690b7051830d23395ee91f9262b54ad4e
IBIRASA <d.ishimwe@alustudent.com>
2025-02-23 22:02:07 +0200

    updated

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -1,30 +1,26 @@
 #!/usr/bin/env python3
-"""python scripts"""
+"""Python script to analyze Nginx logs in MongoDB"""
 from pymongo import MongoClient
-
 
 METHODS = ["GET", "POST", "PUT", "PATCH", "DELETE"]
 
 
-def log_stats(mongo_collection, option=None):
-    """ script that provides some stats about Nginx logs stored in MongoDB
-    """
-    items = {}
-    if option:
-        value = mongo_collection.count_documents(
-            {"method": {"$regex": option}})
-        print(f"\tmethod {option}: {value}")
-        return
-
-    result = mongo_collection.count_documents(items)
-    print(f"{result} logs")
+def log_stats(mongo_collection):
+    """Provides stats about Nginx logs stored in MongoDB"""
+    total_logs = mongo_collection.count_documents({})
+    print(f"{total_logs} logs")
     print("Methods:")
+
     for method in METHODS:
-        log_stats(nginx_collection, method)
+        count = mongo_collection.count_documents({"method": method})
+        print(f"\tmethod {method}: {count}")
+
     status_check = mongo_collection.count_documents({"path": "/status"})
     print(f"{status_check} status check")
 
 
 if __name__ == "__main__":
-    nginx_collection = MongoClient('mongodb://127.0.0.1:27017').logs.nginx
+    client = MongoClient("mongodb://127.0.0.1:27017")
+    nginx_collection = client.logs.nginx
+
     log_stats(nginx_collection)
